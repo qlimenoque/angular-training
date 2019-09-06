037cb3893416be09e54c695a605c9f26ee963598
Vladislav Klimenok <vladyslav.klymenok@jevera.software>
2019-09-06 12:03:05 +0300

    Add route for 'user-info'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './helpers/auth.guard';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
-import { ErrorInterceptor } from './helpers/error.interceptor';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { AboutComponent } from './about/about.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'users', component: UserListComponent },
   { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
-  { path: 'users/edit/:id', component: EditUserComponent, canActivate: [AuthGuard]},
   { path: 'users/add', component: AddUserComponent, canActivate: [AuthGuard]},
+  { path: 'users/:id', component: UserInfoComponent, canActivate: [AuthGuard] },
+  { path: 'users/edit/:id', component: EditUserComponent, canActivate: [AuthGuard]},
   { path: '**', redirectTo: '' }
 ];
 
@@ -35,7 +36,8 @@ export const appRoutingModule = RouterModule.forRoot(routes);
     AddUserComponent,
     UserListComponent,
     EditUserComponent,
-    AboutComponent
+    AboutComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
