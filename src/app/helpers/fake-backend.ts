4c95a2d7ccd7501f41e50044c709c0182c1fc540
Vladislav Klimenok <vladyslav.klymenok@jevera.software>
2019-09-06 09:56:11 +0300

    Delete comments

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -75,7 +75,6 @@ export class FakeBackendInterceptor implements HttpInterceptor {
 }
 
 export let fakeBackendProvider = {
-  // use fake backend in place of Http service for backend-less development
   provide: HTTP_INTERCEPTORS,
   useClass: FakeBackendInterceptor,
   multi: true
