cd7253a7bbc546c967de6687fab2624de2265bcf
Vladislav Klimenok <vladyslav.klymenok@jevera.software>
2019-09-06 18:06:35 +0300

    Remove users loading after successful login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,16 +10,8 @@ import {first} from 'rxjs/operators';
 })
 export class HomeComponent implements OnInit {
   loading = false;
-  users: User[];
-
   constructor(private userService: UserService) { }
 
-  ngOnInit() {
-    this.loading = true;
-    this.userService.getAll().pipe(first()).subscribe(users => {
-      this.loading = false;
-      this.users = users;
-    });
-  }
+  ngOnInit() {}
 
 }
